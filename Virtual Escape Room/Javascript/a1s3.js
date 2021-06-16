1ef892dc4036b4da13cc11e6cf88176000dd0b88
pteritim <79499296+pteritim@users.noreply.github.com>
2021-06-16 22:30:49 +0800

    Update a1s3.js

--- Virtual Escape Room/Javascript/a1s3.js
+++ Virtual Escape Room/Javascript/a1s3.js
@@ -4,7 +4,7 @@ var anchor = document.getElementById("anchor");
 
 function linker (){
   var inputText = document.getElementById("inputbox");
-  if (inputText.value === "For Kate"){
+  if (inputText.value === "For Kate" || inputText.value === "for kate" || inputText.value === "For kate" || inputText.value === "for Kate"){
     anchor.href = "act2scene1.html"
   }
 }
