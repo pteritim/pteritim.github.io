868b83d348a2ab60bd152431d541773f8d783258
pteritim <79499296+pteritim@users.noreply.github.com>
2021-06-24 22:18:44 +0800

    Update a6s1.js

--- Virtual Escape Room/Javascript/a6s1.js
+++ Virtual Escape Room/Javascript/a6s1.js
@@ -1,7 +1,7 @@
 document.getElementById("submitbtn").addEventListener("click",linker);
 
 function linker(){
-  if (document.getElementById("inputbox").value === "23"){
+  if (document.getElementById("inputbox").value === "31"){
     document.getElementById("anchor").href = "act6scene2.html";
   }
 }
