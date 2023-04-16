9d3583b2621d4b174938e21520a8d298bea309d9
pteritim <79499296+pteritim@users.noreply.github.com>
2023-04-16 17:41:45 +0800

    Add files via upload

---
+++ Virtual_Escape_Room/Javascript/a6s1.js
@@ -0,0 +1,7 @@
+document.getElementById("submitbtn").addEventListener("click",linker);
+
+function linker(){
+  if (document.getElementById("inputbox").value === "31"){
+    document.getElementById("anchor").href = "act6scene2.html";
+  }
+}
---
+++ Virtual_Escape_Room/Javascript/prologue.js
@@ -0,0 +1,18 @@
+document.getElementById("submitbtn").addEventListener("click",linker);
+
+var anchor = document.getElementById("anchortag");
+
+function linker (){
+  var inputTime = document.getElementById("inputbox").value;
+  if (inputTime === "01:00"){
+    anchor.href = "act1scene1.html"
+  }
+}
+
+
+document.getElementById("sound").addEventListener("click",play);
+
+function play(){
+  document.getElementById("audio").play();
+  document.getElementById("airplane").style.WebkitAnimationPlayState = "running";
+}
